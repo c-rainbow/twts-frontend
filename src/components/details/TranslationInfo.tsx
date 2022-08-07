2ff655e1cdc3893c5f8c127c4cb3c5620953b19a
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-08-07 00:34:47 -0700

    Basic integration with core backend works

--- src/components/details/TranslationInfo.tsx
+++ src/components/details/TranslationInfo.tsx
@@ -1,9 +1,44 @@
 import { getFullname } from "@/libs/username";
 import { useSelectedFollowInfoStore } from "@/states/follows";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+
+interface TranslationResponse {
+  translated: string;
+  srcLang: string;
+  destLang: string,
+  pronunciation: {
+    text: string,
+    pinyin: string,
+    romaji: string,
+  }
+}
 
 
 function TranslationInfo() {
   const [followInfo] = useSelectedFollowInfoStore(state => [state.followInfo]);
+  const [translation, setTranslation] = useState<string>();
+  const [pinyin, setPinyin] = useState<string>();
+  const [romaji, setRomaji] = useState<string>();
+
+  useEffect(() => {
+    if (!followInfo) {
+      return;
+    }
+    const func = async () => {
+      const response = await axios.post<TranslationResponse>('http://localhost:3001/translate', {
+        displayName: followInfo.followerDisplayName
+      });
+      // response.data
+      setTranslation(response.data.translated);
+      setPinyin(response.data.pronunciation.pinyin);
+      setRomaji(response.data.pronunciation.romaji || '');
+
+    };
+    func();
+  }, [followInfo]);
+
   if (!followInfo) {
     return null;
   }
@@ -21,15 +56,15 @@ function TranslationInfo() {
             <tbody>
               <tr>
                 <td className="font-medium bg-base-200">Translation</td>
-                <td className='bg-base-200 text-left'>trans</td>
+                <td className='bg-base-200 text-left'>{translation}</td>
               </tr>
               <tr>
                 <td className="font-medium bg-base-200">Pinyin</td>
-                <td className='bg-base-200 text-left'>Translation</td>
+                <td className='bg-base-200 text-left'>{pinyin}</td>
               </tr>
               <tr>
                 <td className="font-medium bg-base-200">Romaji</td>
-                <td className='bg-base-200 text-left'>trans</td>
+                <td className='bg-base-200 text-left'>{romaji}</td>
               </tr>
               <tr>
                 <td className="font-medium bg-base-200">Hanja</td>
