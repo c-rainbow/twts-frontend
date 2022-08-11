1837ac3a27aab51d0c4913ad8fef4999630a3cfc
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-08-10 21:05:19 -0700

    API routes

---
+++ src/pages/api/translate/chat.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { TranslateChatResponse } from '@twtts/shared';
+import axios from "axios";
+
+
+const client = axios.create({
+ baseURL: process.env.CORE_TRANSLATION_SERVICE_BASE_URL,
+ headers: {
+  'Content-Type': 'application/json',
+ }
+});
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<TranslateChatResponse>) {
+  if (req.method !== 'POST') {
+    res.status(405);
+    return;
+  }
+  
+  const response = await client.post<TranslateChatResponse>(
+    '/translate/chat', req.body);
+
+  if (response.status >= 400) {
+    res.status(response.status);
+    return;
+  }
+  
+  console.log('response:', response.data);
+  res.send(response.data);
+}
---
+++ src/pages/api/translate/name.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { TranslateNameResponse } from '@twtts/shared';
+import axios from "axios";
+
+
+const client = axios.create({
+ baseURL: process.env.CORE_TRANSLATION_SERVICE_BASE_URL,
+ headers: {
+  'Content-Type': 'application/json',
+ }
+});
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<TranslateNameResponse>) {
+  if (req.method !== 'POST') {
+    res.status(405);
+    return;
+  }
+
+  console.log('body:', req.body);
+  
+  const response = await client.post<TranslateNameResponse>(
+    '/translate/name', req.body);
+  
+  if (response.status >= 400) {
+    res.status(response.status);
+    return;
+  }
+
+  res.send(response.data);
+}
